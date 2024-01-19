e7de77debc298d4f23938521f23c415babc58c32
Wadson <pinheirowadson@gmail.com>
2024-01-18 22:39:49 -0300

    Resolução de Desafio

--- MergulheEmJs/logica-js-projeto_inicial/app.js
+++ MergulheEmJs/logica-js-projeto_inicial/app.js
@@ -1,1 +1,29 @@
-alert('Boas vindas ao jogo do número secreto!');
+alert("Boas vindas ao nosso site!");
+//let nome = "Lua";
+//let idade = 25;
+
+let mensagemDeErro = "Erro! Preencha todos os campos";
+alert(mensagemDeErro);
+let nome = prompt("Digitte seu nome: ");
+let idade = prompt('Digite sua idade: ');
+
+if (idade >= 18) {
+               alert("Pode tirar a habilitação!");
+} else {
+               alert("Só é permitido tirar CNH apartir de 18 anos de idade!");
+}
+
+
+//let saldosaldoDisponivel = 1000;
+//let numeroDeVendas = 50;
+// Sistema de Login
+//let userName = prompt("Digitte seu nome: ");
+//let userAge = prompt("Digite sua idade: ");
+// if (nome == userName && idade == userAge) {
+//                alert('Login feito com sucesso!!');
+//                alert('Seu número de vendas é de ' + numeroDeVendas);
+//                alert('Você possui R$' + saldosaldoDisponivel + " na sua conta.")
+// } else {
+//                alert(' Dados Invalidos!.  Preencha os dados corretamente!')
+
+// }
